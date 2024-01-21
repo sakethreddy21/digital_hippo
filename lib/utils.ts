ae85637b1356c0851abc4f214b988cbfdba121f0
saketh <kk8429@srmist.edu.in>
2024-01-21 16:14:17 +0530

    Add new files and update Cart component

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formatPrice(
+  price: number|string,
+  options:{
+    currency?: "INR"| "USD"| "EUR" | "GBP" |"BDI",
+    notation?: Intl.NumberFormatOptions["notation"],
+  } = { }) {
+  const { currency = "INR", notation = "compact" } = options
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
